47145f0d0ca05f24bc6ccb05bcaa27c97230dd60
Odepax <odepaxforuma@gmail.com>
2018-08-07 22:38:06 +0100

    Revamped presentation website structure

---
+++ Website/Publish-PresentationWebsite.ps1
@@ -0,0 +1,6 @@
+# TODO
+# check for git
+# gather the relevant files
+# publish to gh-pages branch
+# 
+# Also: add a test server to try out the site locally on the phone... @see Odepax/just-name-it
---
+++ Website/pbpw.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+# TODO: @see Publish-PresentationWebsite.ps1
